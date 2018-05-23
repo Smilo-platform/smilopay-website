85fc835058705ad7ebe6f505f8c1bca5876540e6
DionJakobs <dion-jakobs@hotmail.com>
2018-05-23 12:29:48 +0200

    Google Analytics

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -16,4 +16,13 @@
 </footer>
 <script src="/javascript/frontend.js"></script>
 <script src="/javascript/helpers.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119754629-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-119754629-1');
+</script>
+
 </html>
